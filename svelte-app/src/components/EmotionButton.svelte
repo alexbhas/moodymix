2839ca25856aab726cde5aa2d3d42e8e18ce9364
Alex Bhasin <56275911+alexbhasin@users.noreply.github.com>
2023-05-22 11:16:15 -0400

    initial commit

---
+++ svelte-app/src/components/EmotionButton.svelte
@@ -0,0 +1,84 @@
+<script>
+    export let emotion;
+    export let selectedEmotions;
+    export let selectEmotion;
+
+    // Colors for each emotion
+    const colorMap = {
+        Happiness: ["#FCF81D", "#EAE61B"],
+        Relief: ["#F7F591", "#E2E085"],
+        Awe: ["#FF7B25", "#ED7222"],
+        Defiance: ["#FF6A6A", "#E76060"],
+        Anger: ["#FF2626", "#E72222"],
+        Gratitude: ["#F7F591", "#E2E085"],
+        Courage: ["#F6F6BC", "#E5E5B1"],
+        Triumph: ["#FC9959", "#EA8F53"],
+        Regret: ["#F48989", "#DF7D7D"],
+        Jealousy: ["#FF6A6A", "#E76060"],
+        Surprise: ["#FFC85F", "#ECBA59"],
+        Hope: ["#F5D494", "#E6C78B"],
+        Apathy: ["#EAEAEA", "#D8D8D8"],
+        Confusion: ["#CB9AF1", "#BE91E2"],
+        Fear: ["#B96EF4", "#AC6BDF"],
+        Passion: ["#F17ED2", "#DD74C1"],
+        Longing: ["#F0A0DB", "#DC92C9"],
+        Loss: ["#F2DDF6", "#DECAE2"],
+        Betrayal: ["#8A8EED", "#7E82D9"],
+        Despair: ["#4E54F3", "#494FE0"],
+        Love: ["#F44BC6", "#E045B6"],
+        Desire: ["#F17ED2", "#DD74C1"],
+        Nostalgia: ["#EAC8F0", "#D4B6DA"],
+        Melancholy: ["#4E54F3", "#494FE0"],
+        Sadness: ["#0A2FF6", "#0A2AD9"],
+    };
+    $: color = colorMap[emotion][0];
+    $: selectedColor = colorMap[emotion][1];
+
+    // Set font size based on window and text size
+    function getFontSize(emotion) {
+        return window.innerWidth <= 600 && emotion.length > 7
+            ? "0.8em"
+            : emotion.length > 9
+            ? "1em"
+            : "1.2em";
+    }
+
+    $: fontSize = getFontSize(emotion);
+</script>
+
+<button
+    on:click={() => selectEmotion(emotion)}
+    style="background-color: {selectedEmotions.includes(emotion)
+        ? selectedColor
+        : color}; font-size: {fontSize};"
+    class="emotion-button {selectedEmotions.includes(emotion)
+        ? 'selected'
+        : ''}"
+>
+    {emotion}
+</button>
+
+<style>
+    .emotion-button {
+        width: 100%;
+        height: 100%;
+        cursor: pointer;
+        outline: none;
+        transition: background-color 0.3s, transform 0.3s;
+        border-radius: 10px;
+        color: white;
+        will-change: transform;
+        font-family: "Open Sans", sans-serif;
+        font-weight: 700;
+        text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,
+            1px 1px 0 #000;
+    }
+    .emotion-button.selected {
+        transform: rotate(5deg) scale(0.95);
+    }
+    @media (hover: hover) {
+        .emotion-button:not(.selected):hover {
+            transform: scale(1.05);
+        }
+    }
+</style>
---
+++ svelte-app/src/components/EmotionGrid.svelte
@@ -0,0 +1,37 @@
+<!-- src/EmotionGrid.svelte -->
+<script>
+    import EmotionButton from "./EmotionButton.svelte";
+    export let emotions;
+    export let selectedEmotions;
+    export let selectEmotion;
+</script>
+
+<div class="grid">
+    {#each emotions as emotion (emotion)}
+        <div class="grid-item">
+            <EmotionButton {emotion} {selectedEmotions} {selectEmotion} />
+        </div>
+    {/each}
+</div>
+
+<style>
+    .grid {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+        gap: 5px;
+        width: 600px;
+        background-color: black;
+        border-radius: 15px;
+        padding: 15px;
+    }
+
+    .grid-item {
+        aspect-ratio: 1 / 1;
+    }
+    @media (max-width: 600px) {
+        .grid {
+            width: 100%;
+            grid-template-columns: repeat(auto-fill, minmax(80px, 1fr));
+        }
+    }
+</style>
